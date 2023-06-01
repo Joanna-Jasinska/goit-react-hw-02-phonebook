94696038080b4167445f5a0a7a5027321fa90fd5
Joanna <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-06-01 19:40:47 +0200

    Initial commit

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
+
+export const TransactionHistory = ({ transactions }) => (
+  <table className={css['transaction-history']}>
+    <thead>
+      <tr>
+        <th>Type</th>
+        <th>Amount</th>
+        <th>Currency</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      {transactions.map(transaction => (
+        <tr>
+          <td> {_.capitalize(transaction.type)}</td>
+          <td>125</td>
+          <td>USD</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+TransactionHistory.propTypes = {
+  transactions: PropTypes.shape({
+    type: PropTypes.string.isRequired,
+    amount: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+  }),
+};
