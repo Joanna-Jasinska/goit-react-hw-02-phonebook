033d8b893fd3674493399c574586018c4dadbab6
Annaerith <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-06-20 12:51:18 +0200

    removed dead code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import { Phonebook } from './Phonebook/Phonebook';
 
-const INIT = {
-  // contacts: [
-  //   { name: 'Adamian', number: '234-56-78', id: 'adamianid' },
-  //   { name: 'Ania', number: '245-46-85', id: 'aniaid' },
-  //   { name: 'Bavunia', number: '234653', id: 'bavuniaid' },
-  // ],
-  // name: 'n',
-  // number: '999999',
-  // filter: '',
-};
 export class App extends Component {
   constructor() {
     super();
-    this.state = { ...INIT };
   }
   render() {
     return (
       <div className="app">
-        <Phonebook {...this.state} />
+        <Phonebook />
       </div>
     );
   }
