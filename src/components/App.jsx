0507d109fff97907a5e1f377c905ae456c750c82
Annaerith <111195576+Joanna-Jasinska@users.noreply.github.com>
2023-06-20 12:47:18 +0200

    removed useless components, spreads and dead code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,5 @@
 import React, { Component } from 'react';
 import { Phonebook } from './Phonebook/Phonebook';
-// import { Profile } from './Profile/Profile';
-// import { Statistics } from './Statistics/Statistics';
-// import { FriendList } from './FriendList/FriendList';
-// import { TransactionHistory } from './TransactionHistory/TransactionHistory';
-// import user from './../data/user.json';
-// import data from './../data/data.json';
-// import friends from './../data/friends.json';
-// import transactions from './../data/transactions.json';
 
 const INIT = {
   // contacts: [
